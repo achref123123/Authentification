f7449dbfbba3105948704d08b3e66d0a4fbfb439
achref123123 <benkhalifaachref19@gmail.com>
2020-05-11 03:03:00 +0100

    Authentification

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import {Component, OnInit} from '@angular/core';
+import {UserService} from './user.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [UserService]
+})
+export class AppComponent  implements  OnInit{
+  register;
+constructor(private userService: UserService) {
+}
+  ngOnInit() {
+    this.register = {
+      username: '',
+      password: '',
+      email: ''
+    };
+  }
+  registerUser(){
+  this.userService.registerUser(this.register).subscribe(
+    response => {
+      alert('USER ' + this.register.username + 'has been created' );
+    },
+   error => console.log('error', error)
+  );
+
+  }
+}
